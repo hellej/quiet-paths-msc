66b5e12f51ae5294ee4512398b01eda71d5fb55b
Joose Helle <joose.helle@helsinki.fi>
2019-02-16 11:28:47 +0200

    organize files

--- traffic_noise_tests.py
+++ traffic_noise_tests.py
@@ -12,7 +12,7 @@ from matplotlib import pyplot as plt
 
 #%% read traffic noise polygon layer
 noise_polys = gpd.read_file('data/pks_liikennemelu/pks_tieliikenne_LAeq_paiva.shp')
-koskela_poly = gpd.read_file('data/koskela/koskela.shp')
+koskela_poly = gpd.read_file('data/Koskela_input/koskela.shp')
 
 #%% reproject Koskela polygon
 koskela_poly_proj = koskela_poly.to_crs(crs=noise_polys.crs)
@@ -25,10 +25,10 @@ koskela_noises_proj = koskela_noises.to_crs(epsg=3879)
 koskela_noise_polys = geom.explode_multipolygons_to_polygons(koskela_noises_proj)
 print('polygons count', len(koskela_noises_proj.index))
 print('polygons exploded count', len(koskela_noise_polys.index))
-# koskela_noises_proj.to_file('data/Koskela/koskela_noises.shp')
+# koskela_noises_proj.to_file('data/Koskela_output/koskela_noises.shp')
 
 #%%  read walk line
-walk = gpd.read_file('data/Koskela/test_walk_line.shp')
+walk = gpd.read_file('data/Koskela_input/test_walk_line.shp')
 walk_proj = walk.to_crs(epsg=3879)
 walk_geom = walk_proj.loc[0, 'geometry']
 #%% plot noise polygons and walk
@@ -37,14 +37,14 @@ walk_proj.plot(ax=ax)
 
 #%% get noise polygons under the walk line
 polygons_under_line = geom.get_polygons_under_line(walk_geom, koskela_noise_polys)
-polygons_under_line.to_file('data/Koskela/walk_noises.shp')
+polygons_under_line.to_file('data/Koskela_output/walk_noises.shp')
 
 #%% get line split points at polygon boundaries
 split_points = geom.get_line_polygons_inters_points(walk_geom, koskela_noise_polys)
 uniq_split_points = geom.filter_duplicate_split_points(split_points)
 print('split_points count', len(split_points.index))
 print('uniq_split_points count', len(uniq_split_points.index))
-uniq_split_points.to_file('data/Koskela/split_points.shp')
+uniq_split_points.to_file('data/Koskela_output/split_points.shp')
 
 #%% plot line, split points and noise polygons
 ax = koskela_noises_proj.plot()
@@ -56,5 +56,5 @@ split_lines_gdf = geom.split_line_with_polygons(walk_geom, koskela_noise_polys)
 print(split_lines_gdf)
 
 #%%
-split_lines_gdf.to_file('data/Koskela/noise_walks.shp')
+split_lines_gdf.to_file('data/Koskela_output/noise_walks.shp')
 #%%
