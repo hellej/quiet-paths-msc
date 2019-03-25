68871cc49b10bb4f43e112debef934ead030532e
Joose Helle <joose.helle@helsinki.fi>
2019-03-25 07:13:01 +0200

    Faster noise extraction with multiprocessing

--- src/batch_jobs/nw_noises_test/nw_noises_job.sh
+++ src/batch_jobs/nw_noises_test/nw_noises_job.sh
@@ -4,13 +4,13 @@
 #SBATCH -e err.txt
 #SBATCH -n 1
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=15
 #SBATCH --mem-per-cpu=4000
-#SBATCH -t 00:05:00
 #SBATCH –p test
+#SBATCH -t 00:06:00
 #SBATCH --mail-type=END
 #
 
-export OMP_NUM_THREADS=10
+export OMP_NUM_THREADS=15
 module load geoconda
 srun python nw_noises_test.py
--- src/batch_jobs/nw_noises_test/nw_noises_test.py
+++ src/batch_jobs/nw_noises_test/nw_noises_test.py
@@ -23,26 +23,32 @@ print('Edges in the graph:', edge_count)
 edge_dicts[:2]
 
 # EXTRACT NOISES TO SEGMENTS QUICKLY
-start_time = time.time()
-edge_gdf = nw.get_edge_gdf(edge_dicts, ['geometry', 'length', 'uvkey'])
-edge_gdf_sub = edge_gdf[:200].copy()
+def get_segment_noises_df(edge_dicts):
+    edge_gdf_sub = nw.get_edge_gdf(edge_dicts, ['geometry', 'length', 'uvkey'])
+    # add noise split lines as list
+    edge_gdf_sub['split_lines'] = [geom_utils.get_split_lines_list(line_geom, noise_polys) for line_geom in edge_gdf_sub['geometry']]
+    # explode new rows from split lines column
+    split_lines = nw.explode_edges_to_noise_parts(edge_gdf_sub)
+    # join noises to split lines
+    split_line_noises = exps.get_noise_attrs_to_split_lines(split_lines, noise_polys)
+    # aggregate noises back to segments
+    segment_noises = nw.aggregate_segment_noises(split_line_noises)
+    return segment_noises
 
-# add noise split lines as list
-edge_gdf_sub['split_lines'] = [geom_utils.get_split_lines_list(line_geom, noise_polys) for line_geom in edge_gdf_sub['geometry']]
-# explode new rows from split lines column
-split_lines = nw.explode_edges_to_noise_parts(edge_gdf_sub)
-# join noises to split lines
-split_line_noises = exps.get_noise_attrs_to_split_lines(split_lines, noise_polys)
-# aggregate noises back to segments
-segment_noises = nw.aggregate_segment_noises(split_line_noises)
+edge_set = edge_dicts[:1000]
+edge_chunks = utils.get_list_chunks(edge_set, 150)
+pool = Pool(processes=15)
+start_time = time.time()
+
+segment_noise_dfs = pool.map(get_segment_noises_df, edge_chunks)
 
 time_elapsed = round(time.time() - start_time, 1)
-edge_time = round(time_elapsed/200, 3)
-
+edge_time = round(time_elapsed/1000, 3)
 print('\n--- %s seconds ---' % (round(time_elapsed, 1)))
 print('--- %s seconds per edge ---' % (edge_time))
-print(segment_noises.head())
 
-# UPDATE NOISES TO GRAPH
-nw.update_segment_noises(segment_noises, graph_proj)
+#%% UPDATE NOISES TO GRAPH
+for segment_noises in segment_noise_dfs:
+    nw.update_segment_noises(segment_noises, graph_proj)
+
 ox.save_graphml(graph_proj, filename='kumpula_u_g_n.graphml', folder='graphs', gephi=False)
