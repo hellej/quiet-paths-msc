d4f0c5cb1175a22e4af5680a384e6306e67fb167
Joose Helle <joose.helle@helsinki.fi>
2019-04-20 11:19:27 +0300

    small changes

--- src/quiet_paths_app.py
+++ src/quiet_paths_app.py
@@ -20,13 +20,18 @@ CORS(app)
 # READ DATA
 noise_polys = files.get_noise_polygons()
 graph = files.get_network_full_noise()
+print('Data read.')
 # SET NOISE COSTS
 nts = [0.1, 0.125, 0.15, 0.2, 0.25, 0.35, 0.5, 0.75, 1, 1.5, 2, 3, 4, 5, 6]
 nw.set_graph_noise_costs(graph, nts)
-# GET GRAPH DICTS 
+print('Noise costs set.')
+# EXTRACT GRAPH FEATURES
 edge_dicts = nw.get_all_edge_dicts(graph)
 edge_gdf = nw.get_edge_gdf(edge_dicts, ['uvkey', 'geometry'])
 node_gdf = nw.get_node_gdf(graph)
+print('Network features extracted.')
+edges_sind = edge_gdf.sindex
+nodes_sind = node_gdf.sindex
 print('Network ready.')
 
 @app.route('/')
